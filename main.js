9bf5d15b5a4184c4429dff9e14c1396d9e332f9e
Readpato <87768087+Readpato@users.noreply.github.com>
2021-10-25 14:00:12 +0200

    Updated onclick function
    The onclick function that makes all the other content disappear when you click the installator image has been added.

--- main.js
+++ main.js
@@ -10,11 +10,23 @@ $toggleButton.addEventListener('click', () => {
 
 // Function that hides other general information images when and shows respective information when clicked upon
 
+/*
+    Work on branch: Try to put everything a single div so we can just adress it with a single code line
+
+*/
+
 const $installatorImage = document.querySelector('.installator-image');
+const $installatorimagetext = document.querySelector('.installator-image-text');
 const $renovablePlantImage = document.querySelector('.renovable-plant-image');
+const $renovablePlantImageText = document.querySelector('.renovable-plant-image-text');
 const $communityImage = document.querySelector('.community-image');
+const $communityImageText = document.querySelector('.community-image-text');
+
 
 $installatorImage.onclick = function() {
     $renovablePlantImage.classList.toggle('hidden');
+    $renovablePlantImageText.classList.toggle('hidden')
     $communityImage.classList.toggle('hidden');
+    $communityImageText.classList.toggle('hidden');   
 }
+
