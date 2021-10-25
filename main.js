df5c597d1cb11c97a272f1ddcd2311239d07f3a1
Readpato <87768087+Readpato@users.noreply.github.com>
2021-10-25 12:09:51 +0200

    Added onclick function with Installator Image
    The function that hides all other categories when the installator category is selected has been added.
    
    Still missing the function that also hides the text.

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 //Function that allows the togglebutton to display the navigation links.
 
-const toggleButton = document.querySelector('.toggle-button');
-const navigationBarLinks = document.querySelector('.navigationBar-links');
+const $toggleButton = document.querySelector('.toggle-button');
+const $navigationBarLinks = document.querySelector('.navigationBar-links');
 
-toggleButton.addEventListener('click', () => {
-    navigationBarLinks.classList.toggle('active')
+$toggleButton.addEventListener('click', () => {
+    $navigationBarLinks.classList.toggle('active')
 });
+
+
+// Function that hides other general information images when and shows respective information when clicked upon
+
+const $installatorImage = document.querySelector('.installator-image');
+const $renovablePlantImage = document.querySelector('.renovable-plant-image');
+const $communityImage = document.querySelector('.community-image');
+
+$installatorImage.onclick = function() {
+    $renovablePlantImage.classList.toggle('hidden');
+    $communityImage.classList.toggle('hidden');
+}
