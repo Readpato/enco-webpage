f39c539a4263c4093c24a738e8ba372b58a23f15
Readpato <87768087+Readpato@users.noreply.github.com>
2021-10-25 15:38:52 +0200

    Revamped JS functions and added Information Text
    The JavaScript functions that toggle the displays have been written anew for better understanding of the code.
    
    Added as well the ul element hidden on the text that keeps information from the general section.

--- main.js
+++ main.js
@@ -15,18 +15,15 @@ $toggleButton.addEventListener('click', () => {
 
 */
 
-const $installatorImage = document.querySelector('.installator-image');
-const $installatorimagetext = document.querySelector('.installator-image-text');
-const $renovablePlantImage = document.querySelector('.renovable-plant-image');
-const $renovablePlantImageText = document.querySelector('.renovable-plant-image-text');
-const $communityImage = document.querySelector('.community-image');
-const $communityImageText = document.querySelector('.community-image-text');
+const $installatorContainer = document.querySelector('.installator-container');
+const $renewablePlantContainer = document.querySelector('.renewable-plant-container');
+const $renewableCommunityContainer = document.querySelector('.renewable-community-container');
+const $installatorInformationText = document.querySelector('.installator-information-text');
 
-
-$installatorImage.onclick = function() {
-    $renovablePlantImage.classList.toggle('hidden');
-    $renovablePlantImageText.classList.toggle('hidden')
-    $communityImage.classList.toggle('hidden');
-    $communityImageText.classList.toggle('hidden');   
+$installatorContainer.onclick = function() {
+    $renewablePlantContainer.classList.toggle('hidden');
+    $renewableCommunityContainer.classList.toggle('hidden');  
+    $installatorInformationText.classList.toggle('hidden');
+    
 }
 
