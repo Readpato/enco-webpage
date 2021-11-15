d27ff8a228380bdf82c2f46dcf9d3ba910f66226
Readpato <87768087+Readpato@users.noreply.github.com>
2021-11-15 15:55:25 +0100

    Commented JS and CSS Code
    The old main code has been commented to see how it interacts with the new carousel. Added an anchor element on the main page to redirect to the installation page.

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 //Function that allows the togglebutton to display the navigation links.
 
-const $toggleButton = document.querySelector('.toggle-button');
-const $navigationBarLinks = document.querySelector('.navigationBar-links');
+// const $toggleButton = document.querySelector('.toggle-button');
+// const $navigationBarLinks = document.querySelector('.navigationBar-links');
 
-$toggleButton.addEventListener('click', () => {
-    $navigationBarLinks.classList.toggle('active')
-});
+// $toggleButton.addEventListener('click', () => {
+//     $navigationBarLinks.classList.toggle('active')
+// });
 
 
 // Function that hides other general information images when and shows respective information when clicked upon
 
-const $installatorContainer = document.querySelector('.installator-container');
-const $renewablePlantContainer = document.querySelector('.renewable-plant-container');
-const $renewableCommunityContainer = document.querySelector('.renewable-community-container');
-const $installatorInformationText = document.querySelector('.installator-information-text');
-const $communityinformationtext = document.querySelector('.community-information-text');
+// const $installatorContainer = document.querySelector('.installator-container');
+// const $renewablePlantContainer = document.querySelector('.renewable-plant-container');
+// const $renewableCommunityContainer = document.querySelector('.renewable-community-container');
+// const $installatorInformationText = document.querySelector('.installator-information-text');
+// const $communityinformationtext = document.querySelector('.community-information-text');
 
-$installatorContainer.onclick = function() {
-    $renewablePlantContainer.classList.toggle('hidden');
-    $renewableCommunityContainer.classList.toggle('hidden');  
-    $installatorInformationText.classList.toggle('hidden');
+// $installatorContainer.onclick = function() {
+//     $renewablePlantContainer.classList.toggle('hidden');
+//     $renewableCommunityContainer.classList.toggle('hidden');  
+//     $installatorInformationText.classList.toggle('hidden');
     
-}
+// }
 
-$renewableCommunityContainer.onclick = function () {
-    $renewablePlantContainer.classList.toggle('hidden');
-    $installatorContainer.classList.toggle('hidden');
-    $communityinformationtext.classList.toggle('hidden');
-}
+// $renewableCommunityContainer.onclick = function () {
+//     $renewablePlantContainer.classList.toggle('hidden');
+//     $installatorContainer.classList.toggle('hidden');
+//     $communityinformationtext.classList.toggle('hidden');
+// }
 
 
 // * Declarations for the carousel
