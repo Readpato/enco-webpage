53775b0d776d16a74a1ff83c8fbe9466193d791f
Readpato <87768087+Readpato@users.noreply.github.com>
2021-12-21 11:58:54 +0100

    feat: Create form validation functions.
    The functions that validate the user inputs in the fields of the newsletter have been created.
    
    The HTML input elements that compose the form were also given a name attribute to identify then.
    
    The file where all the unitary tests are going to be written has also been created.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-/* Floating Action Button Initialization */
+const $form = document.form;
 
+
+/* Floating Action Button Initialization */
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
@@ -12,14 +14,43 @@ document.addEventListener('DOMContentLoaded', function() {
   /* Form Validators */
 
 
-
   function validateName(userName){
     const regEx = /^[A-z]+$/;
     
     if(userName.length === 0) return 'The name field cannot be empty.';
-    if(regEx.test(userName) === false) return 'The name you inserted is not valid.';
+    if(regEx.test(userName) === false) return 'The name you inserted is invalid. Try again.';
 
-    return false;
-  }
+    return true;
+  };
+
+  function validateLastName(lastName){
+    const regEx = /^[A-z]+$/;
+    
+    if(lastName.length === 0) return 'The last name field cannot be empty.';
+    if(regEx.test(lastName) === false) return 'The last name you inserted is not valid. Try again.';
+
+    return true;
+  };
+
+  function validateEmail(email){
+    const regEx = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    if(regEx.test(email)) return 'The specified email is invalid. Try again.';
+
+    return true;
+  };
+
+  function validateNewsletterForm() {
+    let userName = $form["user-name"];
+    let userLastName = $form["user-last-name"];
+    let userEmail = $form["user-email"];
+
+    validateName(userName);
+    validateLastName(userLastName);
+    validateEmail(userEmail);
+
+  };
+
+
 
 
