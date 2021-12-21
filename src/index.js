badb2b25b951b5a5834dff52f6c6908c58bbcbeb
Readpato <87768087+Readpato@users.noreply.github.com>
2021-12-21 13:09:24 +0100

    feat: Create unitary tests form functions
    The unitary tests that assess if the functions that validate the form for the newsletter have been created.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-
   /* Form Validators */
 
 
@@ -18,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const regEx = /^[A-z]+$/;
     
     if(userName.length === 0) return 'The name field cannot be empty.';
+    if(userName.length >= 20) return "The name of the client is too long.";
     if(regEx.test(userName) === false) return 'The name you inserted is invalid. Try again.';
 
     return true;
@@ -25,17 +25,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function validateLastName(lastName){
     const regEx = /^[A-z]+$/;
-    
     if(lastName.length === 0) return 'The last name field cannot be empty.';
-    if(regEx.test(lastName) === false) return 'The last name you inserted is not valid. Try again.';
+    if(lastName.length >= 20) return "The last name of the client is too long.";
+    if(regEx.test(lastName) === false) return 'The last name you inserted is invalid. Try again.';
 
     return true;
   };
 
   function validateEmail(email){
     const regEx = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
-    if(regEx.test(email)) return 'The specified email is invalid. Try again.';
+    if(email.length === 0) return 'The last email field cannot be empty.';
+    if(regEx.test(email) === false) return 'The specified email is invalid. Try again.';
 
     return true;
   };
@@ -44,13 +44,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let userName = $form["user-name"];
     let userLastName = $form["user-last-name"];
     let userEmail = $form["user-email"];
-
+    
     validateName(userName);
     validateLastName(userLastName);
     validateEmail(userEmail);
 
   };
-
-
-
 
