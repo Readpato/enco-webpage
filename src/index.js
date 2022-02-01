b1c22ae3e415bff9c8619c2bf3aa9ea7967171c1
Readpato <87768087+Readpato@users.noreply.github.com>
2022-02-01 11:30:45 +0100

    refactor: Correctly displays form errors
    The newsletter now correctly displays the errors that may appear in the newsletter

--- src/index.js
+++ src/index.js
@@ -1,94 +1,95 @@
 const $form = document.form;
 
-
 /* Floating Action Button Initialization */
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-        hoverEnabled: false
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems, {
+    hoverEnabled: false,
   });
+});
 
-  /* Form Validators */
+/* Form Validators */
 
-  function validateName(userName){
-    const regEx = /^[A-z]+$/;
-    
-    if(userName.length === 0) return 'The name field cannot be empty.';
-    if(userName.length >= 20) return "The name of the client is too long.";
-    if(regEx.test(userName) === false) return 'The name you inserted is invalid.';
+function validateName(userName) {
+  const regEx = /^[A-z]+$/;
 
-    return '';
+  if (userName.length === 0) return "The name field cannot be empty.";
+  if (userName.length >= 20) return "The name of the client is too long.";
+  if (regEx.test(userName) === false)
+    return "The name you inserted is invalid.";
+
+  return "";
+}
+
+function validateLastName(lastName) {
+  const regEx = /^[A-z]+$/;
+  if (lastName.length === 0) return "The last name field cannot be empty.";
+  if (lastName.length >= 20) return "The last name of the client is too long.";
+  if (regEx.test(lastName) === false)
+    return "The last name you inserted is invalid.";
+
+  return "";
+}
+
+function validateEmail(email) {
+  const regEx =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  if (email.length === 0) return "The email field cannot be empty.";
+  if (regEx.test(email) === false) return "The specified email is invalid.";
+
+  return "";
+}
+
+$form.onsubmit = validateNewsletterForm;
+
+function validateNewsletterForm(event) {
+  let userName = $form["user-name"].value;
+  let userLastName = $form["user-last-name"].value;
+  let userEmail = $form["user-email"].value;
+
+  const errors = {
+    "user-name": validateName(userName),
+    "user-last-name": validateLastName(userLastName),
+    "user-email": validateEmail(userEmail),
   };
 
-  function validateLastName(lastName){
-    const regEx = /^[A-z]+$/;
-    if(lastName.length === 0) return 'The last name field cannot be empty.';
-    if(lastName.length >= 20) return "The last name of the client is too long.";
-    if(regEx.test(lastName) === false) return 'The last name you inserted is invalid.';
+  deletePreviousErrors();
 
-    return '';
-  };
+  const success = errorHandling(errors) === 0;
 
-  function validateEmail(email){
-    const regEx = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if(email.length === 0) return 'The last email field cannot be empty.';
-    if(regEx.test(email) === false) return 'The specified email is invalid.';
+  // if(success) return $form.submit
 
-    return '';
-  };
-
-  $form.onsubmit = validateNewsletterForm;
-
-  function validateNewsletterForm(event) {
-    let userName = $form["user-name"].value;
-    let userLastName = $form["user-last-name"].value;
-    let userEmail = $form["user-email"].value;
-    
-
-    const errors = {
-      "user-name": validateName(userName),
-      "user-last-name": validateLastName(userLastName),
-      "user-email": validateEmail(userEmail)
-    }
-
-    deletePreviousErrors();
-
-    const success = errorHandling(errors) === 0;
-
-    // if(success) return $form.submit
-
-    event.preventDefault();
-};
+  event.preventDefault();
+}
 
 function errorHandling(errors) {
   const error = errors;
   const keys = Object.keys(errors);
   let errorQuantity = 0;
 
-  keys.forEach(function(key){
+  keys.forEach(function (key) {
+    if (error[key]) {
+      $form[key].id = "error";
 
-    if (error[key]){
-      $form[key].id = 'error';
-
-      const $errorList = document.querySelector('.error-list');
-      const $error = document.createElement('li');
+      const $errorList = document.querySelector(".error-list");
+      const $error = document.createElement("li");
       $error.textContent = error[key];
-      $error.className = 'existing-error';
+      $error.className = "existing-error";
       $errorList.appendChild($error);
 
       errorQuantity++;
-
     } else {
-      $form[key].id = '';
+      $form[key].id = "";
     }
   });
   return errorQuantity;
 }
 
-function deletePreviousErrors(){
-  document.querySelectorAll('.existing-error').forEach(function(existingError){
-    existingError.remove();
-  });
+function deletePreviousErrors() {
+  document
+    .querySelectorAll(".existing-error")
+    .forEach(function (existingError) {
+      existingError.remove();
+    });
 }
--- src/unitary-tests.js
+++ src/unitary-tests.js
@@ -1,46 +1,55 @@
 function testNameValidation() {
-    console.assert(validateName('') === 'The name field cannot be empty.',
+  console.assert(
+    validateName("") === "The name field cannot be empty.",
     "The function validateName didn't check if the field of the user name is empty."
-    );
+  );
 
-    console.assert(validateName('AAAAAAAAAAAAAAAAAAAAA') === 'The name of the client is too long.',
+  console.assert(
+    validateName("AAAAAAAAAAAAAAAAAAAAA") ===
+      "The name of the client is too long.",
     "The function validateName didn't check if the name of the user exceeds the maximum characters allowed."
-    );
+  );
 
-    console.assert(validateName('J0hn') === 'The name you inserted is invalid.',
+  console.assert(
+    validateName("J0hn") === "The name you inserted is invalid.",
     "The function validateName didn't check if the user name is invalid."
-    );
-};
+  );
+}
 
 function testLastNameValidation() {
-    console.assert(validateLastName('') === 'The last name field cannot be empty.',
+  console.assert(
+    validateLastName("") === "The last name field cannot be empty.",
     "The function validateLastName didn't check if the field of the last name is empty."
-    );
+  );
 
-    console.assert(validateLastName('AAAAAAAAAAAAAAAAAAAAA') === 'The last name of the client is too long.',
+  console.assert(
+    validateLastName("AAAAAAAAAAAAAAAAAAAAA") ===
+      "The last name of the client is too long.",
     "The function validateLastName didn't check if the last name of the user exceeds the maximum characters allowed."
-    );
+  );
 
-    console.assert(validateLastName('D0e') === 'The last name you inserted is invalid.',
+  console.assert(
+    validateLastName("D0e") === "The last name you inserted is invalid.",
     "The function validateLastName didn't check if the user last name is invalid."
-    );
-};
+  );
+}
 
-function testValidateEmail(){
-    console.assert(validateEmail('') === 'The last email field cannot be empty.',
+function testValidateEmail() {
+  console.assert(
+    validateEmail("") === "The email field cannot be empty.",
     "The function validateEmail didn't check if the email field is empty."
-    );
+  );
 
-    console.assert(validateEmail('patoreadg.com') === 'The specified email is invalid.',
+  console.assert(
+    validateEmail("patoreadg.com") === "The specified email is invalid.",
     "The function validateEmail didn't check if the email is invalid."
-    );
+  );
 }
 
 function unitaryTests() {
-    testNameValidation();
-    testLastNameValidation();
-    testValidateEmail();
-};
+  testNameValidation();
+  testLastNameValidation();
+  testValidateEmail();
+}
 
 unitaryTests();
-
